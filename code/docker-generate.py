62cd48d6983767c8ccde413c539572f8a3fa7b82
Parker Brandt <parker.brandt101@gmail.com>
2022-11-03 22:01:43 -0500

    Added lines for trainer and pokemon parts of docker-compose

--- code/docker-generate.py
+++ code/docker-generate.py
@@ -1,6 +1,5 @@
 
 def getInputParameters():
-
     gridsize = int(input('Size of grid: '))
     numT = int(input('Number of Trainers: '))
     numP = int(input('Number of Pokemon: '))
@@ -19,20 +18,19 @@ def generateDockerComposeYML(numTrainers, numPokemon):
 
     # Create a new machine for each trainer
     for i in range(0, numTrainers):
-        trainerLines=[]
-        dcfile.writelines()
+        trainerLines=['\tclient' + str(i) +':', '\t\tbuild: .', '\t\thostname: trainer' + str(i), '\t\tcontainer_name: Trainer' + str(i), '\t\tnetworks:', '\t\t\t - default']
+        dcfile.writelines(trainerLines)
 
     # Create a new machine for each pokemon
     for i in range(0, numPokemon):
-        pokemonLines=[]
-        dcfile.writelines()
+        pokemonLines=['\tclient' + str(i + numTrainers - 1) +':', '\t\tbuild: .', '\t\thostname: pokemon' + str(i), '\t\tcontainer_name: Pokemon' + str(i), '\t\tnetworks:', '\t\t\t - default']
+        dcfile.writelines(pokemonLines)
 
     # Add the network code at the bottom
     networkLines=['networks', '\tdefault:', '\t\tdriver: bridge']
     dcfile.writelines(networkLines)
 
     dcfile.close()
-
     return
 
 if __name__ == '__main__':
