eb2779670b7b2369e8fc67fded94a5a1371d8475
Parker Brandt <parker.brandt101@gmail.com>
2022-11-10 14:32:45 -0600

    Added function headers for every RPC function

--- code/node.py
+++ code/node.py
@@ -8,8 +8,8 @@ import socket
 import time
 
 import grpc
-import pokemonOU_pb2
-import pokemonOU_pb2_grpc
+import pokemonou_pb2
+import pokemonou_pb2_grpc
 
 
 """
@@ -18,13 +18,12 @@ The Pokemon OU Game class
 The class that implements the functions defined in the pokemonou.proto file
 Most functions will be used for communicating between clients and server
 """
-class PokemonOUGame(pokemonOU_pb2_grpc.PokemonOUServicer):
-    
+class PokemonOUGame(pokemonou_pb2_grpc.PokemonOUServicer):
+    game_board = []
     clients = []
-    people_emojis = []      # https://emojipedia.org/people/
+    people_emojis = []          # https://emojipedia.org/people/
     isused_people_emojis = []
-
-    animal_emojis = []      # https://emojipedia.org/nature/
+    animal_emojis = []          # https://emojipedia.org/nature/
     isused_animal_emojis = []
 
     def __init__(self):
@@ -34,14 +33,26 @@ class PokemonOUGame(pokemonOU_pb2_grpc.PokemonOUServicer):
         return
 
     """
+    Server Services
+    """
+    def Captured(self, request, context):
+        return pokemonou_pb2.Pokemon()
+
+    def Moves(self, request, context):
+        return pokemonou_pb2.MoveList()
+
+    def Show_Board(self, request, context):
+        return pokemonou_pb2.Board()
+
+
+    """
     Trainer & Pokemon Functions
     """
-
     # Called by a client whenever they are first built
     # Will register the client with the server and designate a random emoji to the client based on their type
-    def Initialize(self, client_type):
+    def Initialize(self, request, context):
         emoji = 0
-        if client_type == "trainer":
+        if request.type == "trainer":
             # Choose a random emoji from the people emoji list
             emoji_idx = random.randint(0, len(self.people_emojis) - 1)
             
@@ -52,7 +63,7 @@ class PokemonOUGame(pokemonOU_pb2_grpc.PokemonOUServicer):
             self.isused_people_emojis[emoji_idx] = True
             emoji = self.people_emojis[emoji_idx]
 
-        elif client_type == "pokemon":
+        elif request.type == "pokemon":
             # Choose a random emoji from the animal emoji list
             emoji_idx = random.randint(0, len(self.animal_emojis) - 1)
             
@@ -67,7 +78,38 @@ class PokemonOUGame(pokemonOU_pb2_grpc.PokemonOUServicer):
             # Return -1 to denote an invalid input
             emoji = -1
 
-        return emoji
+        # TODO: Assign location as well on an unoccupied spot on the board
+        # Unoccupied spots are denoted by a 0
+        x = -1;
+        y = -1;
+
+        return pokemonou_pb2.ClientInfo(emojiID=emoji, xLocation=x, yLocation=y)
+
+    
+    def CheckBoard(self, request, context):
+        return pokemonou_pb2.LocationList()
+
+    def Show_Move(self, request, context):
+        return pokemonou_pb2.Move()
+
+    def ShowPath(self, request, context):
+        return pokemonou_pb2.LocationList()
+
+    """
+    Trainer Services
+    """
+    def Capture(self, request, context):
+        return pokemonou_pb2.Pokemon()
+
+    def ShowPokedex(self, request, context):
+        return pokemonou_pb2.Pokedex()
+
+
+    """
+    Pokemon Services
+    """
+    def ShowTrainerInfo(self, request, context):
+        return pokemonou_pb2.Trainer()
 
 
 """
@@ -81,7 +123,7 @@ class Server:
     def serve(self, gridsize):
         server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
         game = PokemonOUGame()
-        pokemonOU_pb2_grpc.add_PokemonOUServicer_to_server(game, server)
+        pokemonou_pb2_grpc.add_PokemonOUServicer_to_server(game, server)
         server.add_insecure_port('[::]:50051')
         server.start()
         
@@ -96,19 +138,26 @@ class Server:
         return
 
 
+
+
 """
 The Pokemon Class
 """
 class Pokemon:
-    port = "50051"
     icon = 0
+    x_loc = 0
+    y_loc = 0
 
     def __init__(self):
-        # Check in with the server, and receive an emoji designation
-
         return
 
     def run(self):
+        with grpc.insecure_channel('server:50051') as channel:
+            stub = pokemonou_pb2_grpc.GuessingGameStub(channel)
+
+            # Initialize this Pokemon with the server, and get an emoji designation
+            self.icon = stub.Initialize()
+
         return
 
 
@@ -116,8 +165,9 @@ class Pokemon:
 The Trainer Class
 """
 class Trainer:
-    port = "50051"
     icon = 0
+    x_loc = 0
+    y_loc = 0
 
     def __init__(self):
         # Check in with the server, and receive an emoji designation
@@ -139,6 +189,8 @@ Decides which class the machine belongs to, and runs that code
 if __name__ == '__main__':
     # Begin logging
     logging.basicConfig()
+
+    # TODO: Parse cla that has board size
 
     # Determine which class the program is
     hostname=re.sub(r'[0-9]', '', socket.gethostname())
